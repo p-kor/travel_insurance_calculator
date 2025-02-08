19b9086328ac76714d7de2101df8f8bdf0883ee7
Pavel Korolev <pasha.korolev@gmail.com>
2025-02-08 16:54:01 +0100

    test: check that AppContext is available

--- src/test/java/com/p_kor/insurance/InsuranceApplicationTests.java
+++ src/test/java/com/p_kor/insurance/InsuranceApplicationTests.java
@@ -2,12 +2,22 @@ package com.p_kor.insurance;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class InsuranceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private final ApplicationContext applicationContext;
+
+    InsuranceApplicationTests(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    @Test
+    void contextLoads() {
+        assertNotNull(applicationContext, "ApplicationContext should not be null");
+    }
 
 }
