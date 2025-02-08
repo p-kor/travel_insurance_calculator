a6e047929eb50e71835c33bdd090e42dff36c4d8
Pavel Korolev <pasha.korolev@gmail.com>
2025-02-08 16:34:02 +0100

    add initil Spring Boot files

---
+++ src/test/java/com/p_kor/insurance/InsuranceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p_kor.insurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
