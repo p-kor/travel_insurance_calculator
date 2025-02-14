7ca8998e4c7b94b82fbee7c845cd15b5e28e6810
Pavel Korolev <pasha.korolev@gmail.com>
2025-02-14 13:41:36 +0100

    feat: refactor TravelCalculatePremiumRequest

--- src/main/java/com/p_kor/insurance/dto/TravelCalculatePremiumRequest.java
+++ src/main/java/com/p_kor/insurance/dto/TravelCalculatePremiumRequest.java
@@ -1,10 +1,17 @@
 package com.p_kor.insurance.dto;
 
+import lombok.*;
+
 import java.time.LocalDate;
 
-public record TravelCalculatePremiumRequest(
-        String personFirstName,
-        String personLastName,
-        LocalDate agreementDateFrom,
-        LocalDate agreementDateTo) {
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public final class TravelCalculatePremiumRequest {
+
+    private String personFirstName;
+    private String personLastName;
+    private LocalDate agreementDateFrom;
+    private LocalDate agreementDateTo;
 }
