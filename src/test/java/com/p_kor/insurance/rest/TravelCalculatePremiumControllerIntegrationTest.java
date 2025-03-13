1d860dd58459a933ed9d7a82d459296c6ca656b2
Pavel Korolev <pasha.korolev@gmail.com>
2025-03-13 23:04:21 +0100

    test: refactor controller integration test to parameterized test

--- src/test/java/com/p_kor/insurance/rest/TravelCalculatePremiumControllerIntegrationTest.java
+++ src/test/java/com/p_kor/insurance/rest/TravelCalculatePremiumControllerIntegrationTest.java
@@ -1,55 +1,64 @@
 package com.p_kor.insurance.rest;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.p_kor.insurance.dto.TravelCalculatePremiumRequest;
-import com.p_kor.insurance.dto.TravelCalculatePremiumResponse;
-import com.p_kor.insurance.testdata.TestDataRequest;
-import org.hamcrest.core.IsNull;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.FieldSource;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import java.math.BigDecimal;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 
-import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class TravelCalculatePremiumControllerIntegrationTest {
 
+    final String BASE_PATH = "rest/";
+    final String ENDPOINT = "/insurance/travel/";
+
+    @Autowired
+    private ResourceLoader resourceLoader;
+
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    private static final List<Arguments> argumentSets = List.of(
+            Arguments.argumentSet("valid request", "valid_request.json", "valid_response.json"),
+            Arguments.argumentSet("missed firstname", "missed_firstname_request.json", "missed_firstname_response.json"));
 
-    @Test
-    void testPostEndPoint() throws Exception {
-        TravelCalculatePremiumRequest request = TestDataRequest.VALID_REQUEST;
-        TravelCalculatePremiumResponse response = TestDataRequest.VALID_RESPONSE;
-        String requestJson;
+    @DisplayName("Integration test for POST /insurance/travel/ endpoint ")
+    @ParameterizedTest(name = "{argumentSetName}")
+    @FieldSource("argumentSets")
+    void testPostEndPointParam(String requestFileName, String responseFileName) throws Exception {
 
-        requestJson = objectMapper.writeValueAsString(request);
+        requestFileName = BASE_PATH + requestFileName;
+        Path requestFile = resourceLoader.getResource("classpath:" + requestFileName).getFile().toPath();
+        String requestJson = Files.readString(requestFile);
 
-        mockMvc
-                .perform(post("/insurance/travel/")
-                        .content(requestJson)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpectAll(
-                        status().isOk(),
-                        content().contentType(MediaType.APPLICATION_JSON_VALUE),
-                        jsonPath("$.personFirstName", is(response.personFirstName())),
-                        jsonPath("$.personLastName", is(response.personLastName())),
-                        jsonPath("$.agreementDateFrom", is(response.agreementDateFrom().toString())),
-                        jsonPath("$.agreementDateTo", is(response.agreementDateTo().toString())),
-                        jsonPath("$.validationErrors", is(IsNull.nullValue())),
-                        jsonPath("$.agreementPrice", is(response.agreementPrice()), BigDecimal.class));
+        responseFileName = BASE_PATH + responseFileName;
+        Path responseFile = resourceLoader.getResource("classpath:" + responseFileName).getFile().toPath();
+        String responseJson = Files.readString(responseFile);
+
+        String actualResponse = mockMvc
+                .perform(post(ENDPOINT)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestJson))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn().getResponse().getContentAsString();
+
+        JSONAssert.assertEquals(responseJson, actualResponse, JSONCompareMode.NON_EXTENSIBLE);
     }
 
 }
