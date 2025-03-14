cec6b1c4fc8218decb804de7533ba7f806702c11
Pavel Korolev <pasha.korolev@gmail.com>
2025-03-14 14:04:43 +0100

    test: refactoring Controller integration test

--- src/test/java/com/p_kor/insurance/rest/TravelCalculatePremiumControllerIntegrationTest.java
+++ src/test/java/com/p_kor/insurance/rest/TravelCalculatePremiumControllerIntegrationTest.java
@@ -3,6 +3,7 @@ package com.p_kor.insurance.rest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+
 import org.junit.jupiter.params.provider.FieldSource;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
@@ -18,7 +19,8 @@ import java.nio.file.Path;
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -34,29 +36,32 @@ class TravelCalculatePremiumControllerIntegrationTest {
     private MockMvc mockMvc;
 
     private static final List<Arguments> argumentSets = List.of(
-            Arguments.argumentSet("valid request", "valid_request.json", "valid_response.json"),
-            Arguments.argumentSet("missed firstname", "missed_firstname_request.json", "missed_firstname_response.json"));
+            Arguments.argumentSet("valid request", "valid_request.json", "valid_response.json", 200),
+            Arguments.argumentSet("missed firstname", "missed_firstname_request.json", "missed_firstname_response.json", 200),
+            Arguments.argumentSet("empty request", "empty_request.json", "empty_response.json", 200));
 
     @DisplayName("Integration test for POST /insurance/travel/ endpoint ")
     @ParameterizedTest(name = "{argumentSetName}")
     @FieldSource("argumentSets")
-    void testPostEndPointParam(String requestFileName, String responseFileName) throws Exception {
+    void testPostEndPointParam(String requestFileName, String responseFileName, int expectedStatus) throws Exception {
 
-        requestFileName = BASE_PATH + requestFileName;
-        Path requestFile = resourceLoader.getResource("classpath:" + requestFileName).getFile().toPath();
+        String requestLocation = "classpath:" + BASE_PATH + requestFileName;
+        Path requestFile = resourceLoader.getResource(requestLocation).getFile().toPath();
         String requestJson = Files.readString(requestFile);
 
-        responseFileName = BASE_PATH + responseFileName;
-        Path responseFile = resourceLoader.getResource("classpath:" + responseFileName).getFile().toPath();
+        String responseLocation = "classpath:" + BASE_PATH + responseFileName;
+        Path responseFile = resourceLoader.getResource(responseLocation).getFile().toPath();
         String responseJson = Files.readString(responseFile);
 
         String actualResponse = mockMvc
                 .perform(post(ENDPOINT)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(requestJson))
-                .andExpect(status().isOk())
+                .andExpect(status().is(expectedStatus))
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON_VALUE))
-                .andReturn().getResponse().getContentAsString();
+                .andReturn()
+                .getResponse()
+                .getContentAsString();
 
         JSONAssert.assertEquals(responseJson, actualResponse, JSONCompareMode.NON_EXTENSIBLE);
     }
