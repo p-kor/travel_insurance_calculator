32f73bc8f94937d541a915b26af9bc2db8e056c0
Pavel Korolev <pasha.korolev@gmail.com>
2025-03-18 21:34:04 +0100

    add logging to controller

--- src/main/java/com/p_kor/insurance/rest/TravelCalculatePremiumController.java
+++ src/main/java/com/p_kor/insurance/rest/TravelCalculatePremiumController.java
@@ -1,10 +1,13 @@
 package com.p_kor.insurance.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.p_kor.insurance.core.TravelCalculatePremiumService;
 import com.p_kor.insurance.dto.TravelCalculatePremiumRequest;
 import com.p_kor.insurance.dto.TravelCalculatePremiumResponse;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,15 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/insurance/travel")
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
+@Slf4j
 class TravelCalculatePremiumController {
 
     private final TravelCalculatePremiumService calculatePremiumService;
+
+    private final ObjectMapper objectMapper;
 
     @PostMapping(
             path = "/",
             consumes = "application/json",
             produces = "application/json")
     public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
+        try {
+            log.info("Received request: {}", objectMapper.writeValueAsString(request));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
         return calculatePremiumService.calculatePremium(request);
     }
 
